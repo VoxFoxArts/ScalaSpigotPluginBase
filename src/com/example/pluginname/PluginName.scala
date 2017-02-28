7d8a6d6e3a796f8db86489abc35395a325f78974
voxfox <voxfoxarts@gmail.com>
2017-02-28 00:28:41 -0800

    initial commit

---
+++ src/com/example/pluginname/PluginName.scala
@@ -0,0 +1,30 @@
+package com.example.pluginname
+
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.command.CommandSender
+import org.bukkit.command.Command
+
+class PluginName extends JavaPlugin {
+
+  override def onEnable {
+    //TODO:
+    /*
+     *
+     */
+  }
+
+  override def onDisable {
+    //TODO:
+    /*
+     *
+     */
+  }
+
+  override def onCommand(sender: CommandSender, cmd: Command, label: String, args: Array[String]) = {
+    //TODO:
+    /*
+     *
+     */
+    false
+  }
+}
